5b115d0507608de48e585e1563685af0a1795b3d
unknown <emckenzie@zoology.ubc.ca>
2020-10-09 19:19:39 -0700

    pivoted longer

--- plateau.R
+++ plateau.R
@@ -1,1 +1,16 @@
+install.packages("tidyverse")
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(Hmisc)
+install.packages("Hmisc")
+
+
 is.data.frame(sqcapfccp)
+str(sqcapfccp)
+
+dime <- pivot_longer(sqcapfccp, cols=c(`sac.area6`, `sac.area7`, `sac.area8`), names_to = "pH", values_to = "area")
+
+str(dime)
+print(dime, n=50)
