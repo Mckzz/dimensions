e431de76ff58cd5bd2ad7b1bdc02476c417abede
unknown <emckenzie@zoology.ubc.ca>
2020-10-14 17:39:56 -0700

    reduced to one pH column, renamed categories to only numbers

--- plateau.R
+++ plateau.R
@@ -18,6 +18,7 @@ dime1c <- pivot_longer(larva1c,
 str(dime1c)
 print(dime1c, n=50)
 
+#so that $pH could be assigned colours
 dime1c$pH <- as.factor(dime1c$pH)
 
 ggplot(data = dime1c, aes(min, area, group = pH)) +
@@ -26,21 +27,36 @@ ggplot(data = dime1c, aes(min, area, group = pH)) +
   theme_classic()
 
 
-####line slope comparisons
+#### line slope comparisons
+rm(widthlong1c)
 
-str(larva1c_combined)
+str(pcts_norm7)
 
 #first pivots the withdths to long, leaving the areas as they were in excel data entry doc
-widthlong1c <- pivot_longer(larva1c_combined, 
+widthlong <- pivot_longer(pcts_norm7, 
                          cols=c(`pH6w`, `pH7w`, `pH8w`), 
-                         names_to = "pH width", values_to = "width")
-#then pivots the areas in widthlong1c 
-slopes1c <- pivot_longer(widthlong1c, 
+                         names_to = "pHwidth", values_to = "width")
+#then pivots the areas in widthlong1c to make the fully long form dataframe
+slopes <- pivot_longer(widthlong, 
                          cols=c(`pH6a`, `pH7a`, `pH8a`), 
-                         names_to = "pH area", values_to = "area")
+                         names_to = "pHarea", values_to = "area")
 
-str(slopes1c)
-print(slopes1c, n=50)
+
+str(slopes)
+print(slopes, n=50)
+
+
+slopes$pHwidth <- as.character(slopes$pHwidth)
+
+slopes$pHwidth[slopes$pHwidth == 'pH6w'] <- '6'
+slopes$pHwidth[slopes$pHwidth == 'pH7w'] <- '7'
+slopes$pHwidth[slopes$pHwidth == 'pH8w'] <- '8'
+slopes$pHwidth <- as.factor(slopes$pHwidth)
+
+slopes$pHarea <- NULL
+
+
+
 
 
 
