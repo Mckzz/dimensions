1352b1bdc36ff1ecb927f7461ca184b70b5fe689
unknown <emckenzie@zoology.ubc.ca>
2020-11-08 18:50:54 -0800

    new file for sline slopes

--- line_slopes.R
+++ line_slopes.R
@@ -1,35 +1,3 @@
-install.packages("tidyverse")
-library(tidyverse)
-library(tidyr)
-library(dplyr)
-library(ggplot2)
-library(Hmisc)
-rm(Hmisc)
-install.packages("Hmisc")
-
-####area plateaus
-
-is.data.frame(larva1e_profiles)
-str(larva1e_profiles)
-
-dime1e <- pivot_longer(larva1e_profiles, 
-                       cols=c(`sac.area6`, `sac.area7`, `sac.area8`), 
-                       names_to = "pH", values_to = "area")
-
-str(dime1e)
-print(dime1e, n=50)
-
-#so that $pH could be assigned colours
-dime1e$pH <- as.factor(dime1e$pH)
-
-ggplot(data = dime1c, aes(min, area, group = pH)) +
-  geom_line(aes(colour = factor(pH))) +
-  labs(x = "min", y = "area") + 
-  theme_classic()
-
-
-
-
 #### line slope comparisons
 rm(norm7)
 str(norm7)
@@ -211,8 +179,8 @@ anova(arfit)
 
 
 measurements <- pivot_longer(slopes, 
-                       cols=c(`width`, `area1`), 
-                       names_to = "measure", values_to = "value")
+                             cols=c(`width`, `area1`), 
+                             names_to = "measure", values_to = "value")
 print(measurements)
 
 fit <- lm(value ~ measure * pH, data = measurements)
